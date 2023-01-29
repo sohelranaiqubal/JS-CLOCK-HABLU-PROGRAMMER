dfcfec36c7bd770502955be4127f0eea57d9d49c
sohelranaiqubal <shoeliqubal7890@gmail.com>
2023-01-29 23:31:15 +0300

    jsone.html

---
+++ jsone.js
@@ -0,0 +1,36 @@
+function myTime() {
+    var myDate = new Date(); 
+    var hr,min = (myDate.getMinutes() < 10) ? "0" + myDate.getMinutes() : myDate.getMinutes(),
+        sec  = (myDate.getSeconds() < 10) ? "0" + myDate.getSeconds() : myDate.getSeconds(),
+        M    = (myDate.getHours() >= 12) ? "PM" : "AM";
+
+        if (myDate.getHours() == 0) {
+            hr = 12;
+        } else if (myDate.getHours() > 12){
+            hr = myDate.getHours() - 12;
+        } 
+        else{
+           hr = myDate.getHours();
+        }
+
+        var currentTime = hr + ":" + min + ":" + sec + ":";
+        
+        document.getElementsByClassName("h")[0].innerHTML = currentTime;
+        document.getElementsByClassName("m")[0].innerHTML = M;
+
+       var myDay = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
+           myMonth = ["January","February","March","April","May","June","July","Augest","September","Octobar","Novembar","Decembar"],
+
+           day = myDate.getDate();
+
+      var currentDate = myDay[myDate.getDay()] + "," + myMonth[myDate.getMonth()] + " " + day;
+          document.getElementsByClassName("date")[0].innerHTML =  currentDate;
+           
+        
+}
+
+myTime();
+
+setInterval( function(){
+    myTime();
+}, 1000);
